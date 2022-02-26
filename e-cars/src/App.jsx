355f18365100e9ebaa7193e55d9aa97b1ad4320b
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-02-26 23:38:00 +0500

    add fonts, img

---
+++ e-cars/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ e-cars/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () =>{
+  return (
+    <div className="header">
+      <div className="container-fluid">
+          
+      </div>
+    
+    </div>
+  )
+}
+
+export default Header;
