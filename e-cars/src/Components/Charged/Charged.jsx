498d070ee82464e27942ecfc732fcc48fb4a72a7
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-02-28 15:19:54 +0500

    add switch page and responsive, and changed

---
+++ e-cars/src/Components/Charged/Charged.jsx
@@ -0,0 +1,33 @@
+import "./Charged.css"
+import GetCharged 
+from "../../Assets/Img/GetCharged.jpg"
+
+const Charged = () =>{
+  return (<>
+    <div className="charged">
+      <div className="container">
+      <div className="charged-left">
+        <h2 className="charged-title">
+        Get Charged.
+        </h2>
+        <p className="charged-text">Find out how to charge an Electric Vehicle
+either on the road or at your home.</p>
+<a href="#" className="charged-link">
+  MORE INFO 
+</a>
+      </div>
+      <div className="charged-right">
+        <img src={GetCharged} alt="GetCharged" className="charged-thumb" />
+      </div>
+
+      </div>
+      
+    </div>
+
+    
+  
+    </>
+  )
+}
+
+export default Charged;
--- e-cars/src/Page/HomePage/HomePage.jsx
+++ e-cars/src/Page/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import Header from "../../Components/Header/Header"
-import SwitchHome from "../../Components/SwitchHome/SwitchHome";
 import "./HomePage.css"
+import Batter from "../../Assets/Img/Batter.png";
+import Charged from "../../Components/Charged/Charged";
 
 const HomePage = () =>{
 return (
@@ -63,10 +64,62 @@ return (
   </div>
  
 
-  {/* <SwitchHome /> */}
+  <section className="section">
+    <div className="container">
+      <div className="section-header">
+        <h2 className="section-title">
+        Why switch to electric?
+        </h2>
+        <p className="section-text">
+        When you switch to an electric vehicle your total cost of ownership goes down considerably.
+        </p>
+      </div>
+      <ul className="section-list">
+        <li className="section-item">
+          <div className="section-month">
+            <div className="month-border">
+            <p className="month-text">
+              SAVE
+            </p>
+            <span className="section-price"> 
+              $50
+            </span>
+            <p>A Month*</p>
+            </div>
+            <span  className="section-subtext">
+            In Fuel Cost
 
- 
-
+            </span>
+          </div>
+        </li>
+        <li className="section-item">
+          <img src={Batter} alt="Batter" 
+          className="section-img" />
+        </li>
+        <li className="section-item">
+          <div className="section-month">
+            <div className="month-border">
+            <p className="month-text">
+              SAVE
+            </p>
+            <span className="section-price">
+              $400
+            </span>
+            <p>A Year*</p>
+            </div>
+            <span  className="section-subtext">
+            In Maintenance
+            </span>
+          </div>
+        </li>
+      </ul>
+      <a href="#" className="section-link">
+        Learn More
+      </a>
+    </div>
+  </section>
+  
+  <Charged />
 </>
 )
 }
